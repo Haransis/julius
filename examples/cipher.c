e89d2a0f475dd760e4ea4bcc707dead4e8f81253
Haran <ara.rahier@gmail.com>
2020-10-30 11:01:55 +0100

    :children_crossing: Add error message if input is too long

--- examples/cipher.c
+++ examples/cipher.c
@@ -7,7 +7,7 @@
 #define MAX_INPUT_SIZE 100
 
 int main(int argc, char *argv []){
-    char *msg = malloc(MAX_INPUT_SIZE);
+    char *msg = malloc(MAX_INPUT_SIZE + 2); // +2 car fgets ajoute un '\n' et NULL
     int shift;
 
     if (argc == 2) {
@@ -15,8 +15,16 @@ int main(int argc, char *argv []){
         if (shift > 0) {
             printf("Enter a string to encrypt (with shift %d)\n", shift);
             fgets(msg, MAX_INPUT_SIZE, stdin);
+            
+            // On retire '\n' s'il y a lieu
             if ((strlen(msg) > 0) && (msg[strlen (msg) - 1] == '\n'))
                 msg[strlen (msg) - 1] = '\0';
+            
+            // On vérifie si l'entrée utilisateur ne dépasse pas la taille autorisée
+            if (!(feof(stdin) || (strlen(msg) != 0 && msg[strlen(msg)-1] == '\n'))){
+                printf("\nToo long message. Maximum length is %d\n", MAX_INPUT_SIZE);
+                return 1;
+            }
             
             encrypt(msg, shift);
             printf("\nResult: \n%s\n", msg);
